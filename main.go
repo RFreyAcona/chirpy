61734c847431915596cd5c2db15430b9eb259660
Raphael Frey <159828193+RaffiFrey@users.noreply.github.com>
2025-12-30 16:10:16 +0100

    Added some click metrics on the file server part of our app

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{}
+
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/healthz", healthzHandler)
-	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	serveMux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	serveMux.HandleFunc("/reset", apiCfg.resetMetricsHandler)
+	handler := http.FileServer(http.Dir(filepathRoot))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(handler)))
 	server := &http.Server{
 		Handler: serveMux,
 		Addr:    ":" + port,
